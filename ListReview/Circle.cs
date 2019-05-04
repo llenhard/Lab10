09cd6658e94179f9fb50c0e57854e898ca3ec74e
imsxz <lukelenhard@gmail.com>
2019-05-04 15:15:04 -0400

    Lab10

---
+++ ListReview/Circle.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListReview
+{
+    class Circle
+    {
+        private double _Radius;
+
+        public Circle(double radius)
+        {
+            _Radius = radius;
+        }
+
+        public double CalculatedCircumference()
+        {
+            return 2 * Math.PI * _Radius;
+        }
+
+        public double CalculateArea()
+        {
+            return Math.PI * _Radius * _Radius;
+        }
+
+        public string CalcualteFormattedCircumference()
+        {
+            return Math.Round(CalculatedCircumference(), 2).ToString();
+        }
+
+        public string CalcualteFormattedArea()
+        {
+            return Math.Round(CalculateArea(), 2).ToString();
+        }
+    }
+}
---
+++ ListReview/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ListReview
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Circle> circles = new List<Circle>();
+            Console.WriteLine("Welcome to the Circle Tester");
+
+            do
+            {
+                Console.Write("Enter radius: ");
+
+                try
+                {
+                    circles.Add(new Circle(double.Parse(Console.ReadLine())));
+                    Console.WriteLine($"Circumference: {circles.Last().CalcualteFormattedCircumference()}" +
+                        $"\nArea: {circles.Last().CalcualteFormattedArea()}");
+                }
+
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid radius! Be sure to enter a valid number.");
+                }
+
+                Console.WriteLine("Would you like to enter another circle?(y/n)");
+            } while (Console.ReadLine().ToLower() == "y");
+
+            Console.WriteLine($"Goodbye. You created {circles.Count} circle(s).");
+        }
+    }
+}
